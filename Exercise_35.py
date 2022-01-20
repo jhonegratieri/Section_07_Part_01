67e81901f395f42425af6a34e07d23a0c41bb426
Jhonatan Gratieri Simoni <jhonatansimoni@gmail.com>
2022-01-20 01:47:06 -0300

    Initial commit

--- Exercise_35.py
+++ Exercise_35.py
@@ -9,21 +9,18 @@ number_b.reverse()
 sum_list = []
 overflow = 0
 
-while True:
-    number_a.append('0')
-    if len(number_a) == 5:
-        break
 
-while True:
-    number_b.append('0')
-    if len(number_b) == 5:
-        break
+while len(number_a) != 5 or len(number_b) != 5:
+
+    if len(number_a) != 5:
+        number_a.append('0')
+
+    if len(number_b) != 5:
+        number_b.append('0')
+
 
 for i in range(5):
-    number_a[i] = int(number_a[i])
-    number_b[i] = int(number_b[i])
-
-    sum_list.append(number_a[i] + number_b[i] + overflow)
+    sum_list.append(int(number_a[i]) + int(number_b[i]) + overflow)
 
     if sum_list[i] >= 10:
         sum_list[i] -= 10
@@ -31,20 +28,18 @@ for i in range(5):
     else:
         overflow = 0
 
-while True:
-    number_a.pop(len(number_a) - 1)
-    if number_a[len(number_a) - 1] != 0:
-        break
 
-while True:
-    number_b.pop(len(number_b) - 1)
-    if number_b[len(number_b) - 1] != 0:
-        break
+while number_a[len(number_a) - 1] == '0' or number_b[len(number_b) - 1] == '0' or sum_list[len(sum_list) - 1] == 0:
 
-while True:
-    sum_list.pop(len(sum_list) - 1)
-    if sum_list[len(sum_list) - 1] != 0:
-        break
+    if number_a[len(number_a) - 1] == '0':
+        number_a.pop(len(number_a) - 1)
+
+    if number_b[len(number_b) - 1] == '0':
+        number_b.pop(len(number_b) - 1)
+
+    if sum_list[len(sum_list) - 1] == 0:
+        sum_list.pop(len(sum_list) - 1)
+
 
 print(f'The vector of the number A is: {number_a}')
 print(f'The vector of the number B is: {number_b}')
